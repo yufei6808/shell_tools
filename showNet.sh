3b197137c0c240b949bf38e78afc8e61c1c6ae46
yufei <yufei6808@163.com>
2017-05-23 15:23:31 +0800

    更新

--- showNet.sh
+++ showNet.sh
@@ -17,18 +17,18 @@ echo  -e  "\t RX `date +%k:%M:%S` TX"
 RX=$((${RXnext}-${RXpre}))
 TX=$((${TXnext}-${TXpre}))
 if [[ $RX -lt 1024 ]];then
-RX="${RX}B/s"
+	RX="${RX}B/s"
 elif [[ $RX -gt 1048576 ]];then
-RX=$(echo $RX | awk '{print $1/1048576 "MB/s"}')
+	RX=$(echo $RX | awk '{print $1/1048576 "MB/s"}')
 else
-RX=$(echo $RX | awk '{print $1/1024 "KB/s"}')
+	RX=$(echo $RX | awk '{print $1/1024 "KB/s"}')
 fi
 if [[ $TX -lt 1024 ]];then
- TX="${TX}B/s"
- elif [[ $TX -gt 1048576 ]];then
- TX=$(echo $TX | awk '{print $1/1048576 "MB/s"}')
- else
- TX=$(echo $TX | awk '{print $1/1024 "KB/s"}')
+ 	TX="${TX}B/s"
+elif [[ $TX -gt 1048576 ]];then
+ 	TX=$(echo $TX | awk '{print $1/1048576 "MB/s"}')
+else
+ 	TX=$(echo $TX | awk '{print $1/1024 "KB/s"}')
 fi
   
 echo -e "$eth \t $RX   $TX "
